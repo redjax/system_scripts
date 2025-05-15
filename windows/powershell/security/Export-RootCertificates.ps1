bc2195ccda97d4ad84ffb784397feddec64625a2
redjax <no@none.com>
2025-05-15 10:03:54 -0400

    Parametrize root script output script

--- windows/powershell/security/Export-RootCertificates.ps1
+++ windows/powershell/security/Export-RootCertificates.ps1
@@ -1,5 +1,14 @@
+Param(
+    [Parameter(Mandatory = $false, HelpMessage = "Path to a directory where the exported root certificates will be saved.")]
+    [string]$OutputPath = "C:\temp",
+    [Parameter(Mandatory = $false, HelpMessage = "Name of the output file for the exported root certificates.")]
+    [string]$OutputFilename = "roots.sst"
+)
+
+$OutputPath = Join-Path -Path $OutputPath -ChildPath $OutputFilename
+
 try {
-    certutil -generateSSTFromWU roots.sst
+    certutil -generateSSTFromWU "$($OutputPath)"
 } catch {
     Write-Error "Error exporting root certificates: $($_.Exception.Message)"
 }
