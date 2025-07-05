7b5d1dc93b59c26d00e726c18c1626c334dd6b50
redjax <5534031+redjax@users.noreply.github.com>
2025-07-05 01:22:03 -0400

    fix(httpie): Add httpi CLI, flatpak desktop app install scripts.

---
+++ linux/bash/installs/httpie/README.md
@@ -0,0 +1,15 @@
+# HTTPie
+
+Install scripts for the cross-platform HTTP client [HTTPiee](https://httpie.io).
+
+## Links
+
+- [HTTPie home](https://httpie.io)
+- [HTTPie github organization](https://github.com/httpie)
+  - [Github: HTTPie desktop repository](https://github.com/httpie/desktop)
+    - [HTTPie desktop latest release](https://github.com/httpie/desktop/releases/latest)
+  - [Github: HTTPie CLI repository](https://github.com/httpie/cli)
+    - [HTTPie CLI latest release](https://github.com/httpie/cli/releases/latest)
+- [HTTPie docs](https://httpie.io/docs)
+  - [HTTPie desktop docs](https://httpie.io/docs/desktop)
+  - [HTTPie CLI docs](https://httpie.io/docs/cli)
---
+++ linux/bash/installs/httpie/install-httpie-desktop.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -e
+
+## Add Flathub repo if missing
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+if [[ $? -ne 0 ]]; then
+echo "[ERROR] Failed to add Flathub repo."
+exit 1
+fi
+
+if ! command -v flatpak &>/dev/null; then
+  echo "Flatpak not found. Please install Flatpak first."
+  exit 1
+fi
+
+## Add Flathub repo if missing
+# Add Flathub repo if missing
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+if [[ $? -ne 0 ]]; then
+  echo "[ERROR] Failed to add Flathub repo."
+  exit 1
+fi
+
+FLATPAK_APP_ID="io.httpie.Httpie"
+
+flatpak install -y flathub "$FLATPAK_APP_ID"
+if [[ $? -ne 0 ]]; then
+  echo "[ERROR] Failed to install HTTPie from Flathub."
+  exit 1
+fi
+
+
+echo "Installed HTTPie desktop flatpak"
+exit 0
