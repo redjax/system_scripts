b986055047ed1d2a131b7be0d6ec7cb034e8b6a6
redjax <5534031+redjax@users.noreply.github.com>
2025-07-22 00:58:08 -0400

    fix(nordvpn): Add nordvpn install scripts

---
+++ linux/bash/installs/nordvpn/install-nordvpn-cli.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -euo pipefail
+
+if command -v nordvpn >/dev/null 2>&1; then
+    echo "NordVPN is already installed."
+    exit 0
+fi
+
+if ! command -v curl >/dev/null 2>&1; then
+    echo "curl is not installed."
+    exit 1
+fi
+
+echo "Installing NordVPN (CLI)"
+sh <(curl -sSf https://downloads.nordcdn.com/apps/linux/install.sh)
+
+exit 0
