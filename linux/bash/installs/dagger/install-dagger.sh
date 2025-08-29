2b28d18d7c9934f580add0f453a3b4b45eed83ac
redjax <5534031+redjax@users.noreply.github.com>
2025-08-29 00:32:11 -0400

    fix(install): Finish install script.
    Add check for dagger is installed.
    
    Add check for curl is installed.
    
    Finish if statement.

--- linux/bash/installs/dagger/install-dagger.sh
+++ linux/bash/installs/dagger/install-dagger.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+if ! command -v curl &>/dev/null; then
+  echo "[ERROR] curl is not installed."
+  exit 1
+fi
+
+if command -v dagger &>/dev/null || type dagger &>/dev/null || which dagger &>/dev/null; then
+  echo "Dagger is already installed."
+  exit 0
+fi
+
+if ! command -v curl &>/dev/null; then
+  echo "[ERROR] curl is not installed."
+  exit 1
+fi
+
 curl -fsSL https://dl.dagger.io/dagger/install.sh | BIN_DIR=$HOME/.local/bin sh
 if [[ $? -ne 0 ]]; then
   echo "[ERROR] Failed installing dagger"
@@ -9,8 +24,12 @@ fi
 if [[ ! -f $HOME/.local/bin/dagger ]]; then
   echo "[ERROR] Curl command ran successfully, but dagger is not in $HOME/.local/bin."
   echo "  If this script was run with sudo, it will be in /usr/local/bin"
+fi
+
+echo ""
+echo "---------------------------------------------------------------------------------"
 echo "Dagger installed to $HOME/.local/bin"
 echo ""
 echo "You may need to add 'export PATH=\$PATH:\$HOME/.local/bin' to your ~/.bashrc file"
-exit 0
 
+exit 0
