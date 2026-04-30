8d282ffb6ec86d5ffa454d5816e93156fecc06b6
redjax <5534031+redjax@users.noreply.github.com>
2026-04-30 12:13:28 -0400

    feat(pwsh): Start work on Powershell install script

---
+++ linux/bash/installs/pwsh/install-powershell.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+############################################
+# Install Microsoft PowerShell from Github #
+# https://github.com/PowerShell/PowerShell #
+############################################
+
+
