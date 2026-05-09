6b9766731c292841218275e08e87d3d08021e60c
redjax <5534031+redjax@users.noreply.github.com>
2026-05-09 00:22:46 -0400

    chore: Add comments

--- linux/bash/git/git_setup/default-git-setup.sh
+++ linux/bash/git/git_setup/default-git-setup.sh
@@ -1,11 +1,20 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+############################################################
+# Script to run my default git setup.                      #
+#                                                          #
+# Prompts for username/email and sets git config defaults. #
+############################################################
+
+## Load git init script
 THIS_DEFAULT_SETUP_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 INIT_SCRIPT="${THIS_DEFAULT_SETUP_DIR}/init-git-setup.sh"
 
+## Load git init script
 source "${INIT_SCRIPT}"
 
+## Set default script arguments
 DEFAULT_ARGS=(
   --default-branch "main"
   --pull-rebase
@@ -15,17 +24,19 @@ DEFAULT_ARGS=(
   --editor "${EDITOR:-nvim}"
 )
 
+## Apply defaults
 parse_args "${DEFAULT_ARGS[@]}"
 
+## Apply user overrides
+parse_args "$@"
+
+## Fill missing required fields
 if [[ -z "${GIT_USERNAME}" ]]; then
   read -r -p "Git username: " GIT_USERNAME
 fi
-
 if [[ -z "${GIT_EMAIL}" ]]; then
   read -r -p "Git email: " GIT_EMAIL
 fi
 
-export GIT_USERNAME
-export GIT_EMAIL
-
+## Call git setup function
 do_git_setup
